1a54feb56b59398b5fcc6ae57805ffe07c8db94e
TheAlchemist75 <Shubhammanur0075@gmail.com>
2023-03-28 00:01:55 +0530

    Finished Resources

--- Frontend/src/components/Resources.js
+++ Frontend/src/components/Resources.js
@@ -39,35 +39,35 @@ function Resources() {
                 <div className="resContent" >
                   <h2 style={{color: "black"}}>Computer Engineering</h2>
                 </div>
-                <img className="resImg" src="https://res.cloudinary.com/dcprg19es/image/upload/v1679941252/Social%20Media/cs1_zkpnqk.png" alt="Image" />
+                <img className="resImg" src="https://res.cloudinary.com/dcprg19es/image/upload/v1679941252/Social%20Media/cs1_zkpnqk.png" alt="CS_Image" />
               </div><br/>
 
               <div className="resBox">
                 <div className="resContent" >
                   <h2 style={{color: "black"}}>IT Engineering</h2>
                 </div>
-                <img className="resImg" src="https://res.cloudinary.com/dcprg19es/image/upload/v1679940949/Social%20Media/it1_nuwvwh.png" alt="Image" />
+                <img className="resImg" src="https://res.cloudinary.com/dcprg19es/image/upload/v1679940949/Social%20Media/it1_nuwvwh.png" alt="IT_Image" />
               </div><br/>
 
               <div className="resBox">
                 <div className="resContent" >
                   <h2 style={{color: "black"}}>Elec. Engineering</h2>
                 </div>
-                <img className="resImg" src="https://res.cloudinary.com/dcprg19es/image/upload/v1679940957/Social%20Media/elec_wq012r.jpg" alt="Image" />
+                <img className="resImg" src="https://res.cloudinary.com/dcprg19es/image/upload/v1679940957/Social%20Media/elec_wq012r.jpg" alt="ELTX_Image" />
               </div><br/>
 
               <div className="resBox">
                 <div className="resContent" >
                   <h2 style={{color: "black"}}>Mech. Engineering</h2>
                 </div>
-                <img className="resImg" src="https://res.cloudinary.com/dcprg19es/image/upload/v1679940963/Social%20Media/mech_irduom.jpg" alt="Image" />
+                <img className="resImg" src="https://res.cloudinary.com/dcprg19es/image/upload/v1679940963/Social%20Media/mech_irduom.jpg" alt="MECH_Image" />
               </div><br/>
 
               <div className="resBox">
                 <div className="resContent" >
                   <h2 style={{color: "black"}}>Civil Engineering</h2>
                 </div>
-                <img className="resImg" src="https://res.cloudinary.com/dcprg19es/image/upload/v1679940953/Social%20Media/civil_yzohr8.jpg" alt="Image" />
+                <img className="resImg" src="https://res.cloudinary.com/dcprg19es/image/upload/v1679940953/Social%20Media/civil_yzohr8.jpg" alt="CIVIL_Image" />
               </div>
             </div><br/>
         </div>
